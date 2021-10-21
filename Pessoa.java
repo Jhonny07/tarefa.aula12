7e242dbcd3d4a8555583022409bff45df4659de1
Jhonny07 <jhonnyjacinto09@icloud.com>
2021-10-21 19:50:29 +0000

    tarefa

---
+++ Pessoa.java
@@ -0,0 +1,43 @@
+public class Pessoa{
+    private String nome;
+    private String telefone;
+    private String endereco;
+// fazer os metodos de acesso público
+// set 
+public void setNome(String nome){
+    this.nome=nome;
+} 
+// get
+public String getNome(){
+    return this.nome;
+}
+//set
+public void setTelefone(String telefone){
+    this.telefone=telefone;
+}
+//get 
+public String getTelefone(){
+    return this.endereco;
+}
+//set
+public void setEndereco(String endereco){
+    this.endereco=endereco;
+}
+//get 
+public String getEndereco(){
+    return this.endereco;
+}
+// Construtor padrão,tem o nome da classe e sem parametros 
+public Pessoa(){
+
+}
+// Construtor com um parametro
+public Pessoa(String nome){
+    this.nome=nome;
+}
+// Construtor com dois parametro 
+public Pessoa(String nome, String telefone){
+    this.nome=nome;
+    this.telefone=telefone; 
+}
+}
