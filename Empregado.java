7fcda09684f0d7180f0e7dbdf4463764bc2f973b
Jhonny07 <jhonnyjacinto09@icloud.com>
2021-10-21 19:56:53 +0000

    tarefa

--- Empregado.java
+++ Empregado.java
@@ -1,3 +1,31 @@
 public class Empregado extends Pessoa{
-    
+
+    private int codigoSetor;
+    private double salarioBase;
+    private double imposto;
+
+//fazer os métodos gets e sets 
+public void setcodigoSetor(String codigoSetor){
+    this.codigoSetor=codigoSetor;
+} 
+// get
+public String getcodigoSetor(){
+    return this.codigoSetor;
+}
+//set
+public void setcodigoSetor(String codigoSetor){
+    this.codigoSetor=codigoSetor;
+}
+//get 
+public String getTelefone(){
+    return this.endereco;
+}
+//set
+public void setEndereco(String endereco){
+    this.endereco=endereco;
+}
+//get 
+public String getEndereco(){
+    return this.endereco;
+}
 }
